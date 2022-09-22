95824937f13ba3548878e3d07b7637e7f53cae5b
Vivek Tripathi <vivektripathi8005@gmail.com>
2022-09-22 09:58:17 +0530

    v_0_1

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+
+const userschema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "Secret h na";
+userschema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+const User = mongoose.model("User", userschema);
+
+app.get("/", (req, res)=>{
+    res.render("home");
+});
+app.get("/login", (req, res)=>{
+    res.render("login");
+});
+app.get("/register", (req, res)=>{
+    res.render("register");
+});
+app.get("/logout", (req, res)=>{
+    res.render("home");
+});
+
+app.post("/register", (req, res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save((err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    });
+});
+app.post("/login", (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username}, (err, foundUser)=>{
+        if(!err){
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
+app.listen(3000, ()=>{
+    console.log("PORT : 3000")
+});
